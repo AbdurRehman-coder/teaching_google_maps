df4e9eb17b67de97cd033482ee86dee0f6cb23ec
Abdur Rehman <abdurrehmanlkl786@gmail.com>
2022-11-17 23:49:59 +0500

    add custom icons to the map

--- lib/custom_marker.dart
+++ lib/custom_marker.dart
@@ -26,33 +26,59 @@ class _CustomMarkersState extends State<CustomMarkers> {
 
   Uint8List? markerIcon;
 
+  List<Marker> listMarkers = [];
+    // Marker(
+    //     markerId: MarkerId('1'),
+    //     position: LatLng(34.1682, 71.7504),
+    //     infoWindow: InfoWindow(
+    //         title: 'We are here',
+    //         anchor: Offset(0.0, 0.9),
+    //         onTap: (){
+    //           print('marker clicked...');
+    //         })
+    // ),
+
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    loadImage();
+
   }
 
-  List<Marker> listMarkers = [
-    Marker(
-        markerId: MarkerId('1'),
-        position: LatLng(34.1682, 71.7504),
-      infoWindow: InfoWindow(
-          title: 'We are here',
-          anchor: Offset(0.0, 0.9),
-      onTap: (){
-        print('marker clicked...');
-      })
-    ),
-    Marker(
-        markerId: MarkerId('2'),
-        position: LatLng(34.0000, 71.4757),
-        infoWindow: InfoWindow(
-            title: 'Islamia College',
-            onTap: (){
-              print('marker clicked...');
-            })
-    ),
+  List<String> imagesPath = [
+    'images/bicycle.png' , 'images/car.png' , 'images/car1.png',
+    'images/car2.png', 'images/delivery-truck.png', 'images/hatchback.png',
+    'images/motorcycle.png'
   ];
+
+  List<LatLng> listCoordinates = [
+    LatLng(33.7008, 72.9682),LatLng(33.6941, 72.9734),
+    LatLng(33.6992, 72.9744), LatLng(33.6939, 72.9771),
+    LatLng(33.7036, 72.9785), LatLng(33.7008, 72.9682),
+    LatLng(34.0000, 71.4757),
+  ];
+
+  loadImage() async{
+
+    for(int i = 0; i < imagesPath.length; i++){
+      print('image path: ${imagesPath[i]}');
+      markerIcon = await getBytesFromAsset(imagesPath[i], 50);
+      listMarkers.add(
+        Marker(
+            markerId: MarkerId(imagesPath[i]),
+             position: listCoordinates[i],
+             icon: BitmapDescriptor.fromBytes(markerIcon!),
+             infoWindow: InfoWindow(title: 'City $i',),
+
+        ),
+      );
+
+    }
+
+  }
+
 
 
   Future<Uint8List> getBytesFromAsset(String path, int width) async {
@@ -68,7 +94,7 @@ class _CustomMarkersState extends State<CustomMarkers> {
 
   @override
   Widget build(BuildContext context) {
-
+    print('marker list: ${listMarkers.length}');
     return Scaffold(
       appBar: AppBar(
           title: Text('Markers'),
